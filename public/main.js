5136fc0341505f595da833555af07282ed710718
aliegeois <pelanman@gmail.com>
2019-04-25 18:53:08 +0200

    thname

--- public/main.js
+++ public/main.js
@@ -5,15 +5,15 @@ onload = () => {
 		let thead = document.createElement('thead'),
 			thId = document.createElement('th'),
 			thEmoji = document.createElement('th'),
-			//thName = document.createElement('th'),
+			// thName = document.createElement('th'),
 			thElo = document.createElement('th');
 		thId.innerHTML = 'id';
 		thEmoji.innerHTML = 'Emoji';
-		//thName.innerHTML = 'Nom';
+		// thName.innerHTML = 'Nom';
 		thElo.innerHTML = 'Elo';
 		thead.appendChild(thId);
 		thead.appendChild(thEmoji);
-		//thead.appendChild(thName);
+		// thead.appendChild(thName);
 		thead.appendChild(thElo);
 		table.appendChild(thead);
 
@@ -28,7 +28,7 @@ onload = () => {
 			let img = new Image(64, 64);
 			img.src = `https://cdn.discordapp.com/emojis/${emoji.id}`;
 			tdEmoji.appendChild(img);
-			//tdName.innerHTML = emoji.???
+			// tdName.innerHTML = emoji.???
 			tdElo.innerHTML = emoji.elo;
 			tr.appendChild(tdId);
 			tr.appendChild(tdEmoji);
